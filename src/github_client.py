502e5c00e24cdd1149f98bdbd22468818d23626c
caixintong <caixintong3@jd.com>
2025-01-15 19:49:52 +0800

    feat:支持until参数

--- src/github_client.py
+++ src/github_client.py
@@ -18,6 +18,47 @@ class GitHubClient:
             'pull_requests': self.fetch_pull_requests(repo, since, until)  # 获取拉取请求
         }
         return updates
+    def fetch_data(self, url, since=None, until=None, state='closed', sort='updated', direction='asc'):
+        # 通用的数据获取方法，支持分页和时间范围过滤。
+        # :param url: API 地址
+        # :param since: 起始时间
+        # :param until: 结束时间
+        # :param state: 数据状态（open、closed、all）
+        # :param sort: 排序字段
+        # :param direction: 排序方向（asc 或 desc）
+        # :return: 过滤后的数据列表
+        data_list = []
+        page = 1
+
+        while True:
+            params = {
+                'state': state,
+                'since': since,
+                'sort': sort,
+                'direction': direction,
+                'page': page,
+                'per_page': 100  # 每页最多获取100条记录
+            }
+            response = requests.get(url, headers=self.headers, params=params)
+            response.raise_for_status()
+            data = response.json()
+            if not data:
+                break  # 如果没有数据，说明已经获取完所有页
+            # 如果 until 参数存在，过滤掉超过 until 时间的数据
+            if until:
+                data = [item for item in data if item['updated_at'] <= until]
+            data_list.extend(data)
+            # 如果最后一页的最后一条记录的更新时间超过 until，停止分页
+            if until and data and data[-1]['updated_at'] > until:
+                break
+            
+            # 如果当前页的数据量小于 per_page，说明没有更多数据了，停止分页
+            if len(data) < 100:
+                break
+
+            page += 1
+
+        return data_list
 
     def fetch_commits(self, repo, since=None, until=None):
         # https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28#list-commits
@@ -34,32 +75,12 @@ class GitHubClient:
         return response.json()  # 返回JSON格式的数据
 
     def fetch_issues(self, repo, since=None, until=None):
-        url = f'https://api.github.com/repos/{repo}/issues'  # 构建获取问题的API URL
-        # todo: 
-        # 1.结合 since 参数，获取在某个时间点之后创建的问题。在客户端代码中根据 created_at 或 updated_at 字段进行过滤
-        # 2.GitHub 的 GraphQL API 提供了更灵活的查询方式，可以通过自定义查询实现时间范围过滤。
-        params = {
-            'state': 'closed',  # 仅获取已关闭的问题
-            'since': since,
-            'until': until
-        }
-        response = requests.get(url, headers=self.headers, params=params)
-        response.raise_for_status()
-        return response.json()
+        url = f'https://api.github.com/repos/{repo}/issues'
+        return self.fetch_data(url, since, until, state='closed')
 
     def fetch_pull_requests(self, repo, since=None, until=None):
-        url = f'https://api.github.com/repos/{repo}/pulls'  # 构建获取拉取请求的API URL
-         # todo: 
-        # 1.结合 since 参数，获取在某个时间点之后创建的问题。在客户端代码中根据 created_at 或 updated_at 字段进行过滤
-        # 2.GitHub 的 GraphQL API 提供了更灵活的查询方式，可以通过自定义查询实现时间范围过滤。
-        params = {
-            'state': 'closed',  # 仅获取已合并的拉取请求
-            'since': since,
-            'until': until
-        }
-        response = requests.get(url, headers=self.headers, params=params)
-        response.raise_for_status()
-        return response.json()
+        url = f'https://api.github.com/repos/{repo}/pulls'
+        return self.fetch_data(url, since, until, state='closed')
 
     def export_daily_progress(self, repo):
         today = datetime.now().date().isoformat()  # 获取今天的日期
